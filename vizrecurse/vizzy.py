1508946f7147ed0a3601a02a14d29ec0a3e5a01c
CodyPedersen <cody.pedersen@proton.me>
2024-05-25 19:17:25 -0400

    Simplify frame discernment logic

--- vizrecurse/vizzy.py
+++ vizrecurse/vizzy.py
@@ -8,6 +8,8 @@ from typing import Callable, ParamSpec, TypeVar
 
 import networkx as nx
 
+from .debug import dump_frame_info
+
 G = nx.DiGraph()
 
 Param = ParamSpec("Param")
@@ -49,28 +51,30 @@ def visualize(func: Callable[Param, RetType]) -> Callable[Param, RetType]:
         )
         G.add_node(**cur_fingerprint)
 
-        # if not coming from main context, build edge
-        if prev_frame.f_locals.get('__name__') != '__main__':
+        # We've hit module frame context
+        if prev_frame.f_code.co_name == '<module>':
+            return func(*args, **kwargs)
 
-            prev_wrapped_frame = prev_frame.f_back
-            if not prev_wrapped_frame:
-                raise ValueError("Wrapper frame not found. Validate frame context.")
+        # Get parent wrapped frame
+        prev_wrapped_frame = prev_frame.f_back
+        if not prev_wrapped_frame:
+            raise ValueError("Wrapper frame not found. Validate frame context.")
 
-            # If in wrapper context, add edge
-            if prev_wrapped_frame.f_code.co_name == cur_frame.f_code.co_name:
+        # If in wrapper frame context, add edge
+        if prev_wrapped_frame.f_code.co_name == cur_frame.f_code.co_name:
 
-                # Construct representation from previous stack frame
-                prev_wrapped_fingerprint = generate_node_fingerprint(
-                    prev_wrapped_frame,
-                    prev_frame.f_code.co_name,
-                    str(prev_wrapped_frame.f_locals['args']),
-                    prev_wrapped_frame.f_locals['fn_timestamp']
-                )
+            # Construct representation from previous stack frame
+            prev_wrapped_fingerprint = generate_node_fingerprint(
+                prev_wrapped_frame,
+                prev_frame.f_code.co_name,
+                str(prev_wrapped_frame.f_locals['args']),
+                prev_wrapped_frame.f_locals['fn_timestamp']
+            )
 
-                G.add_edge(
-                    prev_wrapped_fingerprint["node_for_adding"],
-                    cur_fingerprint["node_for_adding"]
-                )
+            G.add_edge(
+                prev_wrapped_fingerprint["node_for_adding"],
+                cur_fingerprint["node_for_adding"]
+            )
 
         return func(*args, **kwargs)
 
