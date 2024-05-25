f2fc6c80c0dab988762fa303904b4db020793d43
CodyPedersen <cody.pedersen@proton.me>
2024-05-25 17:08:35 -0400

    feature: display updates

--- vizrecurse/display.py
+++ vizrecurse/display.py
@@ -10,19 +10,23 @@ from .vizzy import G
 
 
 def draw(
+    figsize: tuple[int,int] = (16,10),
     font_size: int = 6,
     font_weight: str = "bold",
     gv_layout: str = "dot",
-    arrow_size: int = 7,
-    node_size: int = 70
+    node_size: int = 500
     ) -> None:
     """Draw recursive tree/list from captured graph"""
 
+    plt.figure(figsize=figsize)
+
+    # Generate positions and labels
     pos = graphviz_layout(G, prog=gv_layout)
     labels = {node: data['label'] for node, data in G.nodes(data=True)}
 
-    nx.draw_networkx_nodes(G, pos, node_size=node_size)
-    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrowstyle='-|>', arrowsize=arrow_size)
+    # Draw figure
+    nx.draw_networkx_nodes(G, pos, node_size=node_size, node_shape='o')
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrowstyle='-|>', arrowsize=7)
     nx.draw_networkx_labels(
         G,
         pos,
