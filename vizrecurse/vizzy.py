7284bbe0d35ae348d7ae96e469530325328ff6d4
CodyPedersen <cody.pedersen@proton.me>
2024-06-15 20:17:17 -0400

    Tracing: Update to event-based architecture (#10)
    * change to event-based viz structure to allow more thorough debugging
    
    * change to event-based viz structure to allow more thorough debugging
    
    * change to event-based viz structure to allow more thorough debugging.
    
    * change to event-based viz structure to allow more thorough debugging.

--- vizrecurse/vizzy.py
+++ vizrecurse/vizzy.py
@@ -1,10 +1,18 @@
-# pylint: disable=invalid-name, import-error
+# pylint: disable=import-error,invalid-name,unused-argument
 """Recursion visualization toolkit"""
-import inspect
+import sys
 import time
 
 from types import FrameType
-from typing import Callable, ParamSpec, TypeVar
+from typing import (
+    Any,
+    Concatenate,
+    Callable,
+    Optional,
+    ParamSpec,
+    TypeVar
+)
+from typing_extensions import TypeAlias
 
 import networkx as nx
 
@@ -12,6 +20,8 @@ G = nx.DiGraph()
 
 Param = ParamSpec("Param")
 RetType = TypeVar("RetType")
+TraceFunction: TypeAlias = 'Callable[[FrameType, str, Any], Optional[TraceFunction]]'
+
 
 def generate_node_fingerprint(
     frame: FrameType,
@@ -30,44 +40,64 @@ def generate_node_fingerprint(
     }
 
 
+def get_frame_set(
+    frame: Optional[FrameType]
+) -> tuple[Optional[FrameType], Optional[FrameType]]:
+    """Capture current and prior frameset"""
+
+    cur_frame = frame
+
+    # unable to locate prior frame state
+    if not frame or frame.f_code.co_name != 'debugged_func':  # remove hardcode
+        return None, None
+
+    # initialize to prior frame
+    frame = frame.f_back
+    if not frame:
+        return frame, None
+
+    # locate last debug frame
+    while frame.f_code.co_name != 'debugged_func':  # remove hardcode
+        prior_frame = frame.f_back
+
+        if not prior_frame or prior_frame.f_code.co_name == '<module>':
+            return cur_frame, None
+
+        frame = prior_frame
+
+    return cur_frame, frame
+
+
 def visualize(func: Callable[Param, RetType]) -> Callable[Param, RetType]:
-    """Decorator for visualization of recursive calls"""
+    """Test debugging the existing frame"""
 
-    def inner(*args: Param.args, **kwargs: Param.kwargs) -> RetType:
-        # function signature for traceability
-        fn_timestamp = time.time_ns()
+    def trace_callback(frame: FrameType, event: str, _: Any) -> TraceFunction:
+        """Callback function for system trace"""
 
-        cur_frame = inspect.currentframe()
-        if not cur_frame:
-            raise ValueError("Unable to infer existing frame.")
+        cur_debug_frame, prior_debug_frame = get_frame_set(frame)
 
-        prev_frame = cur_frame.f_back
-        if not prev_frame:
-            raise ValueError("Unable to infer previous frame.")
+        if not cur_debug_frame:
+            return trace_callback
 
-        cur_fingerprint = generate_node_fingerprint(
-            cur_frame, func.__name__, str(args), fn_timestamp
-        )
-        G.add_node(**cur_fingerprint)
+        if event == 'call':
+            # Add node
+            fn_args = str(cur_debug_frame.f_locals['args'])
+            print(fn_args)
+            cur_fingerprint = generate_node_fingerprint(
+                frame, func.__name__, fn_args, frame.f_locals['fn_timestamp']
+            )
+            print(f"cur_fingerprint {cur_fingerprint}")
+            G.add_node(**cur_fingerprint)
 
-        # We've hit module frame context
-        if prev_frame.f_code.co_name == '<module>':
-            return func(*args, **kwargs)
+            if not prior_debug_frame:
+                return trace_callback
 
-        # Get parent wrapped frame
-        prev_wrapped_frame = prev_frame.f_back
-        if not prev_wrapped_frame:
-            raise ValueError("Wrapper frame not found. Validate frame context.")
-
-        # If in wrapper frame context, add edge
-        if prev_wrapped_frame.f_code.co_name == cur_frame.f_code.co_name:
-
-            # Construct representation from previous stack frame
+            # Add edge
             prev_wrapped_fingerprint = generate_node_fingerprint(
-                prev_wrapped_frame,
-                prev_frame.f_code.co_name,
-                str(prev_wrapped_frame.f_locals['args']),
-                prev_wrapped_frame.f_locals['fn_timestamp']
+                prior_debug_frame,
+                prior_debug_frame.f_locals['func'].__name__,
+                str(prior_debug_frame.f_locals['args']),
+                prior_debug_frame.f_locals['fn_timestamp']
             )
 
             G.add_edge(
@@ -75,6 +105,31 @@ def visualize(func: Callable[Param, RetType]) -> Callable[Param, RetType]:
                 cur_fingerprint["node_for_adding"]
             )
 
-        return func(*args, **kwargs)
+        return trace_callback
 
-    return inner
+
+    def initialize_debug(
+            func: Callable[Concatenate[int, Param], RetType]
+    ) -> Callable[Param, RetType]:
+        """Set trace and inject timestamp to uniquely ID function calls."""
+
+        def wrapper(*args: Param.args, **kwargs: Param.kwargs) -> RetType:
+            timestamp = time.time_ns()
+            sys.settrace(trace_callback)
+            res = func(timestamp, *args, **kwargs)
+            sys.settrace(None)
+            return res
+        return wrapper
+
+    @initialize_debug
+    def debugged_func(
+            fn_timestamp: int, /,
+            *args: Param.args,
+            **kwargs: Param.kwargs,
+        ) -> RetType:
+        """Dummy wrapper with injected functionality"""
+
+        res = func(*args, **kwargs)
+        return res
+
+    return debugged_func
