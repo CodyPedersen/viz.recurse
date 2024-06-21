b66dc03ca6f8d6b7e04541380da7caf59f19b088
CodyPedersen <cody.pedersen@proton.me>
2024-06-20 20:47:14 -0400

    Implement line-level debugging and minor refactors (#11)
    * implement line debugging and refactor
    
    * implement line debugging and refactor.

--- vizrecurse/vizzy.py
+++ vizrecurse/vizzy.py
@@ -1,5 +1,5 @@
-# pylint: disable=import-error,invalid-name,unused-argument
 """Recursion visualization toolkit"""
+import linecache
 import sys
 import time
 
@@ -14,13 +14,16 @@ from typing import (
 )
 from typing_extensions import TypeAlias
 
-import networkx as nx
+import networkx as nx  # pylint:disable=import-error
 
-G = nx.DiGraph()
 
 Param = ParamSpec("Param")
-RetType = TypeVar("RetType")
+RetType = TypeVar("RetType")  # pylint: disable=invalid-name
 TraceFunction: TypeAlias = 'Callable[[FrameType, str, Any], Optional[TraceFunction]]'
+
+# globals (to be refactored)
+G = nx.DiGraph()
+container: Optional[str] = None
 
 
 def generate_node_fingerprint(
@@ -48,7 +51,7 @@ def get_frame_set(
     cur_frame = frame
 
     # unable to locate prior frame state
-    if not frame or frame.f_code.co_name != 'debugged_func':  # remove hardcode
+    if not frame or frame.f_code.co_name != container:  # remove hardcode
         return None, None
 
     # initialize to prior frame
@@ -57,7 +60,7 @@ def get_frame_set(
         return frame, None
 
     # locate last debug frame
-    while frame.f_code.co_name != 'debugged_func':  # remove hardcode
+    while frame.f_code.co_name != container:  # remove hardcode
         prior_frame = frame.f_back
 
         if not prior_frame or prior_frame.f_code.co_name == '<module>':
@@ -74,19 +77,33 @@ def visualize(func: Callable[Param, RetType]) -> Callable[Param, RetType]:
     def trace_callback(frame: FrameType, event: str, _: Any) -> TraceFunction:
         """Callback function for system trace"""
 
+        # trace logging for function
+        frame_fn = frame.f_code.co_name
+        if not frame_fn.startswith('_initializer') and frame_fn != container:
+            fname = frame.f_code.co_name
+
+            filename, line_num = frame.f_code.co_filename, frame.f_lineno
+            line_code = linecache.getline(filename, line_num).strip()
+            if event == 'line':
+                execution_log = f"{filename}:{fname}:{line_num} - {line_code}"
+                print(execution_log)
+            elif event == 'call':
+                execution_log = f"{filename}:{line_num} - Called {fname}"
+                print(execution_log)
+
         cur_debug_frame, prior_debug_frame = get_frame_set(frame)
 
+        # short-circuit if not reference frame
         if not cur_debug_frame:
             return trace_callback
 
+        # call event logic
         if event == 'call':
             # Add node
             fn_args = str(cur_debug_frame.f_locals['args'])
-            print(fn_args)
             cur_fingerprint = generate_node_fingerprint(
                 frame, func.__name__, fn_args, frame.f_locals['fn_timestamp']
             )
-            print(f"cur_fingerprint {cur_fingerprint}")
             G.add_node(**cur_fingerprint)
 
             if not prior_debug_frame:
@@ -105,31 +122,42 @@ def visualize(func: Callable[Param, RetType]) -> Callable[Param, RetType]:
                 cur_fingerprint["node_for_adding"]
             )
 
+
         return trace_callback
 
 
-    def initialize_debug(
+    def _initializer(
             func: Callable[Concatenate[int, Param], RetType]
     ) -> Callable[Param, RetType]:
         """Set trace and inject timestamp to uniquely ID function calls."""
 
-        def wrapper(*args: Param.args, **kwargs: Param.kwargs) -> RetType:
+        global container  # pylint: disable=global-statement
+        container = func.__name__
+
+        def _initializer_inner(*args: Param.args, **kwargs: Param.kwargs) -> RetType:
             timestamp = time.time_ns()
             sys.settrace(trace_callback)
             res = func(timestamp, *args, **kwargs)
             sys.settrace(None)
             return res
-        return wrapper
 
-    @initialize_debug
+        return _initializer_inner
+
+    @_initializer
     def debugged_func(
-            fn_timestamp: int, /,
+            fn_timestamp: int, /,  # pylint: disable=unused-argument
             *args: Param.args,
             **kwargs: Param.kwargs,
         ) -> RetType:
-        """Dummy wrapper with injected functionality"""
+        """
+        Dummy wrapper with injected functionality. Used as 
+        a fixed reference point for tracking the stack.
+        Must be wrapped with initializer decorator to capture 
+        first function call & inject fn reference timestamp.
+        """
 
         res = func(*args, **kwargs)
+
         return res
 
     return debugged_func
